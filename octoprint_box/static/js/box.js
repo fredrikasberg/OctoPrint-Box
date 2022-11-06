9b7b8f33bf4940cd8ff5fefa460d4c7b29dc7cca
Fredrik Åsberg <fredrik@aringinnovation.se>
2022-11-06 10:57:33 +0100

    Initial commit

---
+++ octoprint_box/static/js/box.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-Box
+ *
+ * Author: Fredrik Åsberg
+ * License: AGPLv3
+ */
+$(function() {
+    function BoxViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: BoxViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_box, #tab_plugin_box, ...
+        elements: [ /* ... */ ]
+    });
+});
